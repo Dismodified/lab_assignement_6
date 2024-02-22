7583158b1c1e77fe6772374896b7cc306091e47d
Dismodified <160194877+Dismodified@users.noreply.github.com>
2024-02-22 09:20:37 -0500

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -10,7 +10,7 @@ int search(int numbers[], int low, int high, int value) {
 	if (numbers[mid] == value) {
     	    return mid;
 	} else if (numbers[mid] > value) {
-            return searech(numbers, low, mid - 1, value);
+            return search(numbers, low, mid - 1, value);
 	} else {
             return search(numbers, mid + 1, high, value);
 	}
